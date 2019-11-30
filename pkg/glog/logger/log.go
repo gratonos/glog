9527cea99570d54d2378727ae5d06747362a6ede
gratonos <gratonos@outlook.com>
2019-11-30 09:47:59 +0800

    feat(logger): add methods for integer types

--- pkg/glog/logger/log.go
+++ pkg/glog/logger/log.go
@@ -18,17 +18,43 @@ func getLog(logger *Logger) *Log {
 }
 
 func (this *Log) Int(key string, value int) *Log {
-	if this == nil {
-		return nil
-	}
+	return this.appendInt64Field(key, int64(value))
+}
 
-	this.appendSeparator()
+func (this *Log) Int8(key string, value int8) *Log {
+	return this.appendInt64Field(key, int64(value))
+}
 
-	this.buf = append(this.buf, key...)
-	this.buf = append(this.buf, ':')
-	this.buf = strconv.AppendInt(this.buf, int64(value), 10)
+func (this *Log) Int16(key string, value int16) *Log {
+	return this.appendInt64Field(key, int64(value))
+}
 
-	return this
+func (this *Log) Int32(key string, value int32) *Log {
+	return this.appendInt64Field(key, int64(value))
+}
+
+func (this *Log) Int64(key string, value int64) *Log {
+	return this.appendInt64Field(key, value)
+}
+
+func (this *Log) Uint(key string, value uint) *Log {
+	return this.appendUint64Field(key, uint64(value))
+}
+
+func (this *Log) Uint8(key string, value uint8) *Log {
+	return this.appendUint64Field(key, uint64(value))
+}
+
+func (this *Log) Uint16(key string, value uint16) *Log {
+	return this.appendUint64Field(key, uint64(value))
+}
+
+func (this *Log) Uint32(key string, value uint32) *Log {
+	return this.appendUint64Field(key, uint64(value))
+}
+
+func (this *Log) Uint64(key string, value uint64) *Log {
+	return this.appendUint64Field(key, value)
 }
 
 func (this *Log) Commit() {
@@ -63,6 +89,34 @@ func (this *Log) appendRuntimeInfo(file string, line int, fn string) {
 
 func (this *Log) fillTimestamp(tm time.Time) {
 	tm.AppendFormat(this.buf[:0], timeLayout)
+}
+
+func (this *Log) appendInt64Field(key string, value int64) *Log {
+	return this.appendField(key, func() {
+		this.buf = strconv.AppendInt(this.buf, value, 10)
+	})
+}
+
+func (this *Log) appendUint64Field(key string, value uint64) *Log {
+	return this.appendField(key, func() {
+		this.buf = strconv.AppendUint(this.buf, value, 10)
+	})
+}
+
+func (this *Log) appendField(key string, appender func()) *Log {
+	if this == nil {
+		return nil
+	}
+
+	this.appendSeparator()
+
+	this.buf = append(this.buf, '(')
+	this.buf = append(this.buf, key...)
+	this.buf = append(this.buf, ": "...)
+	appender()
+	this.buf = append(this.buf, ')')
+
+	return this
 }
 
 func (this *Log) appendNewLine() {
